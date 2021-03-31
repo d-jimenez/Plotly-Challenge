7bc46a46771578025efa5b9e30e344c2ccad582f
David Jimenez <davjim94@gmail.com>
2021-03-31 00:26:07 -0600

    Updated bar chart to change on selection of subject id, finished initital bubble chart, need to automate bubble chart and create Demographic Info Table.

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,4 @@
-
-
-  // Initializes the page with a default plot and drop down list
+// Initializes the page with a default plot and drop down list
 function init() {
   // Load json data as d3 object
   d3.json("././data/samples.json").then(data=>{
@@ -8,10 +6,6 @@ function init() {
     var subj_ids = data.names;
     var metadata = data.metadata;
     var samples = data.samples;
-
-    console.log(subj_ids);
-    console.log(metadata);
-    console.log(samples);
 
     // Insert the OTU IDs into the selection tag as options
     d3.select("select")
@@ -36,16 +30,16 @@ function init() {
     }];
 
     var layout_bar = {
-      title: ` Top 10 OTUs found in Test Subject: ${samples[0].id}`,
+      title: ` Top 10 OTUs found in Test Subject`,
     };
 
     Plotly.newPlot('bar', data_bar,layout_bar);
 
     // Create Bubble Chart
-
     var data_bubble = [{
       x: samples[0].otu_ids,
       y: samples[0].sample_values,
+      text:samples[0].otu_labels.slice(0,10),
       mode: 'markers',
       marker: {
         size: samples[0].sample_values,
@@ -57,33 +51,38 @@ function init() {
   })
 };
 
-// // Call updatePlotly() when a change takes place to the DOM
-// d3.selectAll("#selDataset").on("change", updatePlotly);
+// Call updatePlotly() when a change takes place to the DOM
+d3.selectAll("#selDataset").on("change", updatePlotly);
 
-// // This function is called when a dropdown menu item is selected
-// function updatePlotly() {
-//   // Use D3 to select the dropdown menu
-//   var dropdownMenu = d3.select("#selDataset");
-//   // Assign the value of the dropdown menu option to a variable
-//   var sel_sample = dropdownMenu.property("value");
+// This function is called when a dropdown menu item is selected
+function updatePlotly() {
+  // Load json data as d3 object
+  d3.json("././data/samples.json").then(data=>{
+    // Grab values from the response json object to build the plots
+    var subj_ids = data.names;
+    var metadata = data.metadata;
+    var samples = data.samples;
 
-//   // Initialize x and y arrays
-//   var x = [];
-//   var y = [];
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var sel_sample = dropdownMenu.property("value");
+ 
+    // Console log the first 10 oyt_ids and sample_values based on selection
+    console.log(samples[(subj_ids.indexOf(sel_sample))].otu_ids.slice(0,10))
+    console.log(samples[(subj_ids.indexOf(sel_sample))].sample_values.slice(0,10))
 
-//   if (dataset === 'dataset1') {
-//     x = [1, 2, 3, 4, 5];
-//     y = [1, 2, 4, 8, 16];
-//   }
+    // Restyle the bar chart based on the text subject id selection
+    // This process can be improved, this assumes the names are in outlined in the same order in the samples array
+    Plotly.restyle("bar", "x", [samples[(subj_ids.indexOf(sel_sample))].sample_values.slice(0,10).reverse()]);
+    Plotly.restyle("bar", "y", [samples[(subj_ids.indexOf(sel_sample))].otu_ids.slice(0,10).map(otu=>`OTU ${otu}`)]);
+    Plotly.restyle("bar", "text", [samples[(subj_ids.indexOf(sel_sample))].otu_labels.slice(0,10)]);
 
-//   else if (dataset === 'dataset2') {
-//     x = [10, 20, 30, 40, 50];
-//     y = [1, 10, 100, 1000, 10000];
-//   }
+    //Restyle Bubble Chart Based on Selection
 
-//   // Note the extra brackets around 'x' and 'y'
-//   Plotly.restyle("plot", "x", [x]);
-//   Plotly.restyle("plot", "y", [y]);
-// }
+    // Update Demographic info table based on selection
+
+  }); 
+}
 
 init();
